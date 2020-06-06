4f710315cf2a5b74761f0247ff9ed28eb18624a8
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-06-06 16:37:51 +0700

    Updates

---
+++ precache-manifest.268e83468c80205bda2a6079c86d65f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92b8b2f8ce709bb7543e0c8a3a74265",
+    "url": "/film-booking-website/index.html"
+  },
+  {
+    "revision": "959d8a2915475989f158",
+    "url": "/film-booking-website/static/css/main.c59c342a.chunk.css"
+  },
+  {
+    "revision": "a9f8015daf65c41c05d9",
+    "url": "/film-booking-website/static/js/2.99dffcf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/film-booking-website/static/js/2.99dffcf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959d8a2915475989f158",
+    "url": "/film-booking-website/static/js/main.c15f186e.chunk.js"
+  },
+  {
+    "revision": "7b8796cd4d3966da2e51",
+    "url": "/film-booking-website/static/js/runtime-main.25409ca9.js"
+  }
+]);
